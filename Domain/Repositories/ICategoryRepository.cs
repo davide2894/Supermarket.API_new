9d07666ad0bd37381e475a6569692aaa6fb87496
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2021-10-17 16:02:59 +0200

    correct namespace - part 2

--- Domain/Repositories/ICategoryRepository.cs
+++ Domain/Repositories/ICategoryRepository.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Supermarket.API.Domain.Models;
+using Supermarket.APi_new.Domain.Models;
 
 namespace Supermarket.APi_new.Domain.Repositories
 {
