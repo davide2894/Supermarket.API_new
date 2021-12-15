40397eabdb7938e7ba1b7eaedc5039b320052c86
davide2894 <davide.iaiunese@gmail.com>
2021-12-15 12:16:50 +0100

    product endpoint - added enum extension

--- Mapping/ModelToResource.cs
+++ Mapping/ModelToResource.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Supermarket.API_new.Domain.Models;
 using Supermarket.API_new.Resources;
+using Supermarket.API_new.Extensions;
 
 namespace Supermarket.API_new.Mapping
 {
@@ -9,6 +10,10 @@ namespace Supermarket.API_new.Mapping
         public ModelToResource()
         {
             CreateMap<Category, CategoryResource>();
+
+            CreateMap<Product, ProductResource>()
+                .ForMember(src => src.UnitOfMeasurement,
+                    opt => opt.MapFrom(src => src.UnitOfMeasurement.ToDescriptionString()));
         }
     }
 }
